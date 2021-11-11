27533c0a652dd813a0b421ea190f80ead6c616d3
mvanvuren <m.vuren@upcmail.nl>
2021-11-11 14:54:18 +0100

    fire and forget

--- dzVents/scripts/global_data.lua
+++ dzVents/scripts/global_data.lua
@@ -4,15 +4,15 @@ return {
             local text = subject .. ' ' .. message
             local previousText = domoticz.variables('SMS').value
             if (string.sub(previousText, 1, 3) ~= string.sub(text, 1, 3)) then -- save some money ;-)
-                domoticz.executeShellCommand("/root/scripts/sms.sh '" .. text .. "'")
+                domoticz.executeShellCommand("(/root/scripts/sms.sh '" .. text .. "')&")
                 domoticz.variables('SMS').set(text)
             end
         end,
         snapshotFront = function(domoticz)
-            domoticz.executeShellCommand('/root/scripts/front.sh')
+            domoticz.executeShellCommand('(/root/scripts/front.sh)&')
         end,
         snapshotGarden = function(domoticz)
-            domoticz.executeShellCommand('/root/scripts/garden.sh')
+            domoticz.executeShellCommand('(/root/scripts/garden.sh)&')
         end
     }
 }
