696afe1799c68e1f43487f72dc735af517c9ef09
mvanvuren <m.vuren@upcmail.nl>
2021-11-11 20:34:36 +0100

    correct casing

--- python/report-crash.py
+++ python/report-crash.py
@@ -6,7 +6,7 @@ REPORT = f"{DOMO_PATH}/domoticz_crash.log"
 SUBJECT = 'domoticz crash report'
 MAILTO = os.environ['MAILTO']
 
-if os.path.exists(report):
+if os.path.exists(REPORT):
     command = f"mutt -a {REPORT} -s \"{SUBJECT}\" -- {MAILTO} </dev/null"
     os.system(command)
-    os.remove(report)
+    os.remove(REPORT)
